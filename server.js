d03e33523f27cafda6fbf78f75b8af14f54a94a1
suruchi soni <suruchi810910@gmail.com>
2025-07-02 15:28:29 +0530

    origin-allow

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express()
 const port = 4000
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({ origin: "https://foodzone-frontend-hutg.onrender.com" }));
 
 //DB connection
 connectDB();
